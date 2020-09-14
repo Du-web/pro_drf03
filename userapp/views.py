9272bc2bff176e9712c83a50a0411c9f9197c751
dudu <648302938@qq.com>
2020-09-14 20:48:27 +0800

    将模型注册到后台admin站点，并在view文件创建员工类视图

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 
 # Create your views here.
+from rest_framework.views import APIView
+
+
+class EmployeeAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+
+        return Response({
+            'status': 200,
+            'message': '查询成功'
+        })
