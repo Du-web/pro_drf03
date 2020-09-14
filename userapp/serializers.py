27b7a35a276b55ccc1e29fb9aee309a8f96fa5f7
dudu <648302938@qq.com>
2020-09-14 21:52:14 +0800

    给序列化类自定义gender字段和pic字段

--- userapp/serializers.py
+++ userapp/serializers.py
@@ -7,5 +7,15 @@ from pro_drf03 import settings
 class EmployeeSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
-    gender = serializers.IntegerField()
-    pic = serializers.ImageField()
+    # gender = serializers.IntegerField()
+    # pic = serializers.ImageField()
+    gender = serializers.SerializerMethodField()
+    pic = serializers.SerializerMethodField()
+
+    def get_gender(self, obj):
+
+        return obj.get_gender_display()
+
+    def get_pic(self, obj):
+
+        return f'{"http://127.0.0.1:8000"}{settings.MEDIA_URL}{obj.pic}'
