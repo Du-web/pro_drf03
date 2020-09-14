cc8fffb8fc3acff67239e14dee434cb984fab1e8
dudu <648302938@qq.com>
2020-09-14 21:14:22 +0800

    在view文件的员工类视图添加get方法

--- userapp/urls.py
+++ userapp/urls.py
@@ -4,4 +4,5 @@ from userapp import views
 
 urlpatterns = [
     path('user/', views.EmployeeAPIView.as_view()),
+    path('user/<str:id>/', views.EmployeeAPIView.as_view()),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -3,12 +3,27 @@ from rest_framework.response import Response
 
 # Create your views here.
 from rest_framework.views import APIView
+from userapp.models import Employee
+from userapp.serializers import EmployeeSerializer
 
 
 class EmployeeAPIView(APIView):
     def get(self, request, *args, **kwargs):
+        user_id = kwargs.get('id')
+        if user_id:
+            emp = Employee.objects.get(pk=user_id)
+            emp_ser = EmployeeSerializer(emp).data
 
-        return Response({
-            'status': 200,
-            'message': '查询成功'
-        })
+            return Response({
+                'status': 200,
+                'message': '查询单个成功',
+                'results': emp_ser
+            })
+        else:
+            emps = Employee.objects.all()
+            emps_ser = EmployeeSerializer(emps, many=True).data
+            return Response({
+                'status': 200,
+                'message': '查询所有成功',
+                'results': emps_ser
+            })
