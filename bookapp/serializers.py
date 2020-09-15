34e1dc19db4272e78450f16ab94645f3163831a1
dudu <648302938@qq.com>
2020-09-15 22:08:21 +0800

    在serializers文件中制定序列化器和反序列化器的整合

--- bookapp/serializers.py
+++ bookapp/serializers.py
@@ -34,13 +34,49 @@ class BookModelDeSerializer(ModelSerializer):
         model = Book
         fields = ('book_name', 'price', 'publish', 'authors')
         extra_kwargs = {
-            'book_name':{
+            'book_name': {
                 'max_length': 18,
                 'min_length': 2,
             },
             'price': {
                 'required': True,
                 'decimal_places': 2,
+            }
+        }
+
+    def validate(self, attrs):
+        name = attrs.get('book_name')
+        book = Book.objects.filter(book_name=name)
+        if len(book) > 0:
+            raise exceptions.ValidationError('图书名已存在')
+        return attrs
+
+    def validate_price(self, obj):
+        if obj > 100:
+            raise exceptions.ValidationError('图书的价格不能查过100元')
+        return obj
+
+
+class BookModelSerializerV2(ModelSerializer):
+    """
+    序列化器与反序列化器整合
+    """
+    class Meta:
+        model = Book
+        fields = ('book_name', 'price', 'pic', 'publish', 'authors ')
+        extra_kwargs = {
+            'book_name': {
+                'max_length': 18,
+                'min_length': 2,
+            },
+            'publish': {
+                'write_only': True,
+            },
+            'authors': {
+                'write_only': True,
+            },
+            'pic': {
+                'read_only': True
             }
         }
 
